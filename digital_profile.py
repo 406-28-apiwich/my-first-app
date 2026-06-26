8fa6f4f44b77ed0f8d61f0925e944c57be3c210b
406-28-apiwich <54590@yupparaj.ac.th>
2026-06-26 10:29:10 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("⭐ My Digital Profile")
+
+st.header("🧩 เเนะนำตัว")
+st.write("นายอภิวิชญ์ ญาณวดี")
+st.write("ชื่อเล่น ต้นหนาว")
+st.write("เลขที่ 28 ม.4/6")
+
+st.header("🎯 เป้าหมายเเละสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย: อยากเรียนด้านการเเพทย์")
+st.write("สิ่งที่อยากเป็นในอนาคต:จิตเเพทย์ (psychiatrist)")
