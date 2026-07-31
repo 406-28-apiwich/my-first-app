a2a3a7d149e7424c087f3577d465d6dec636785b
406-28-apiwich <54590@yupparaj.ac.th>
2026-07-31 10:59:12 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -19,6 +19,8 @@ if st.button("Calculate BMI 📈"):
    st.success("⭐ Healthy")
  elif 23.0 <= bmi < 25.0:
    st.success("ℹ️ Fat")
+ elif 1000 <= bmi < 1000000000000:
+   st.success("Are you still alive?")
  else:
    st.error("🚨 Very fat")
 
