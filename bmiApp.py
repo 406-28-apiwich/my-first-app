203f8ba9e8cc20f43d8f7e6950772976f509f161
406-28-apiwich <54590@yupparaj.ac.th>
2026-07-31 10:46:58 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -7,8 +7,8 @@ weight = st.number_input("Weight 🏋️ (KG):")
 height_cm = st.number_input("Height 📏 (CM):")
 
 if st.button("Calculate BMI 📈"):
-height_m = height_cm / 100
-bmi = weight / (height_m ** 2)
-
-st.write("---")
-st.header(f"YOUR BMI IS: **{bmi:.2f}**")
+ height_m = height_cm / 100
+ bmi = weight / (height_m ** 2)
+ 
+ st.write("---")
+ st.header(f"YOUR BMI IS: **{bmi:.2f}**")
