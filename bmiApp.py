711c6a67b69b325f2ddeb44446a068a1dcf005a6
406-28-apiwich <54590@yupparaj.ac.th>
2026-07-31 10:45:04 +0700

    Create bmiApp.py

---
+++ bmiApp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.markdown("# :red[💪BMI CALCULATOR]")
+st.write("Type in your Weight & Height")
+
+weight = st.number_input("Weight 🏋️ (KG):")
+height_cm = st.number_input("Height 📏 (CM):")
+
+if st.button("Calculate BMI 📈"):
+height_m = height_cm / 100
+bmi = weight / (height_m ** 2)
+
+st.write("---")
+st.header(f"YOUR BMI IS: **{bmi:.2f}**")
