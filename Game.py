e74b685e8106836e3be82969278808a3b87ae6e8
406-28-apiwich <54590@yupparaj.ac.th>
2026-08-28 10:27:16 +0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -52,7 +52,7 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     
-     if u_ans3 == "back":
+    if u_ans3 == "back":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
